82ee50303b3ea0f372454da9742e19d93258b409
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-09-05 12:44:09 +0100

    first commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
