cb577c9992f47752a8ad511e3dee0f5c96bb5ec0
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-09-06 16:56:13 +0100

    Applying tailwind styles

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Jaccey</div>;
+  return <div className="hidden text-sm font-semibold">Jaccey</div>;
 }
 
 export default Username;
